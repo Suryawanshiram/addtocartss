e43706d713fbe9e433c3f3543158a800e102d430
Ram B. Suryawanshi <101462022+Suryawanshiram@users.noreply.github.com>
2022-10-20 09:33:28 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          CopyRight & Content policy website 2022 All rigths reversed by suryawanshiram2@gmail.com.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
